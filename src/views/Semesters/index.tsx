48bc8bd7c5fd5bfa29a052e33d422093c4343b33
Victor <victorescalonag@gmail.com>
2024-11-25 10:15:15 -0500

    [FEATURE] Actualizado hook de semestres

--- src/views/Semesters/index.tsx
+++ src/views/Semesters/index.tsx
@@ -1,61 +1,8 @@
-import { AiOutlineEye } from 'react-icons/ai';
-import AtButton from '../../components/AtButton';
 import { BasicLayout } from '../../layouts/BasicLayout';
 import { useSemesters } from './useSemesters';
-import { Link } from 'react-router-dom';
 
 export const Semesters = () => {
-	const { semesters, isLoading } = useSemesters('daniel+student');
-
-	const renderSemesterList = (filterActive: boolean, title: string) => {
-		const filteredSemesters = semesters.filter(
-			(semester) => semester.semester_status === filterActive
-		);
-
-		return (
-			<div className='inactive-list w-full'>
-				<h2 className='font-medium pb-4'>{title}</h2>
-				<div className='list flex flex-col gap-2 h-full max-h-[calc(73vh-140px)] overflow-y-auto'>
-					{isLoading && (
-						<AtButton
-							variant={filterActive ? 'primary' : 'white'}
-							className='flex items-center w-[330px] justify-center'
-						>
-							Loading...
-							<span className='text-[1.7rem] pl-4'>
-								<AiOutlineEye />
-							</span>
-						</AtButton>
-					)}
-					{!isLoading && filteredSemesters.length === 0 && (
-						<AtButton
-							variant={filterActive ? 'transparent' : 'white'}
-							className='w-full'
-						>
-							{filterActive ? 'No Active Semesters' : 'No Closed Semesters'}
-						</AtButton>
-					)}
-					{!isLoading &&
-						filteredSemesters.map((semester) => (
-							<Link
-								key={semester.semester_id}
-								to={`/my-courses/${semester.semester_id}`}
-							>
-								<AtButton
-									variant={filterActive ? 'primary' : 'white'}
-									className='flex items-center w-[330px] justify-center'
-								>
-									{semester.semester_name}
-									<span className='text-[1.7rem] pl-4'>
-										<AiOutlineEye />
-									</span>
-								</AtButton>
-							</Link>
-						))}
-				</div>
-			</div>
-		);
-	};
+	const { renderSemesterList } = useSemesters('daniel+student');
 
 	return (
 		<BasicLayout className='!items-start !pt-12'>
--- src/views/Semesters/useSemesters.tsx
+++ src/views/Semesters/useSemesters.tsx
@@ -1,10 +1,63 @@
 import { useEffect, useState } from 'react';
 import { Semester } from '../../api/types';
 import { getSemesters } from '../../api/services';
+import AtButton from '../../components/AtButton';
+import { AiOutlineEye } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 export const useSemesters = (email: string) => {
 	const [semesters, setSemesters] = useState<Semester[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const renderSemesterList = (filterActive: boolean, title: string) => {
+		const filteredSemesters = semesters.filter(
+			(semester) => semester.semester_status === filterActive
+		);
+
+		return (
+			<div className='inactive-list w-full'>
+				<h2 className='font-medium pb-4'>{title}</h2>
+				<div className='list flex flex-col gap-2 h-full max-h-[calc(73vh-140px)] overflow-y-auto'>
+					{isLoading && (
+						<AtButton
+							variant={filterActive ? 'primary' : 'white'}
+							className='flex items-center w-[330px] justify-center'
+						>
+							Loading...
+							<span className='text-[1.7rem] pl-4'>
+								<AiOutlineEye />
+							</span>
+						</AtButton>
+					)}
+					{!isLoading && filteredSemesters.length === 0 && (
+						<AtButton
+							variant={filterActive ? 'transparent' : 'white'}
+							className='w-full'
+						>
+							{filterActive ? 'No Active Semesters' : 'No Closed Semesters'}
+						</AtButton>
+					)}
+					{!isLoading &&
+						filteredSemesters.map((semester) => (
+							<Link
+								key={semester.semester_id}
+								to={`/my-courses/${semester.semester_id}`}
+							>
+								<AtButton
+									variant={filterActive ? 'primary' : 'white'}
+									className='flex items-center w-[330px] justify-center'
+								>
+									{semester.semester_name}
+									<span className='text-[1.7rem] pl-4'>
+										<AiOutlineEye />
+									</span>
+								</AtButton>
+							</Link>
+						))}
+				</div>
+			</div>
+		);
+	};
 
 	const handleGetSemesters = async () => {
 		setIsLoading(true);
@@ -21,5 +74,5 @@ export const useSemesters = (email: string) => {
 		handleGetSemesters();
 	}, [email]);
 
-	return { semesters, isLoading };
+	return { semesters, isLoading, renderSemesterList };
 };

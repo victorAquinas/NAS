0f96d6d52a8d8d381a680b4fda5e673275f474f8
Victor <victor.it@aquinasnetwork.com>
2025-01-23 15:40:40 -0500

    [FEATURE] Se modifica componente del evento del calendario, ahora es mas descriptivo y más fácil de ver

--- src/components/AtCalendarEvent/index.tsx
+++ src/components/AtCalendarEvent/index.tsx
@@ -1,16 +1,25 @@
 import { CalendarEvent } from '../../views/CalendarPage/types';
+import { PracticaPlaceTypeName } from '../../api/types';
 
 interface AtCalendarEventProps {
 	event: CalendarEvent;
 }
 export const AtCalendarEvent = ({ event }: AtCalendarEventProps) => {
 	return (
-		<div className='text-[0.65rem] p-1 transition duration-200 ease-in-out hover:scale-[1.02] cursor-pointer calendar-event-container'>
-			<p className='font-medium'>{event.title}</p>
-			<p>
-				{event.shift} -{' '}
-				<span className='font-semibold'>{event.group_name}</span>
+		<div
+			className={`text-[0.65rem] p-1 transition duration-200 ease-in-out hover:scale-[1.02] cursor-pointer calendar-event-container`}
+		>
+			<p
+				className={`font-medium text-xxs  w-max px-1 rounded-sm ${
+					event.type === PracticaPlaceTypeName.IN_SITE
+						? 'bg-yellow-500 text-secondary'
+						: 'bg-secondary text-white'
+				} `}
+			>
+				{event.type}
 			</p>
+			<p className='font-semibold'>{event.group_name}</p>
+			<p>{event.shift}</p>
 		</div>
 	);
 };
